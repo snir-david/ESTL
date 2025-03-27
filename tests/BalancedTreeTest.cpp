21448bb4f62262769c7716b440f15002909acc59
snir-david <snirdavid1994@gmail.com>
2025-03-27 09:39:25 +0200

    Test|ESTL|Adding multithread test for balancedTrees

--- tests/BalancedTreeTest.cpp
+++ tests/BalancedTreeTest.cpp
@@ -1,20 +1,28 @@
 //
 // Created by SnirN on 3/26/2025.
 //
-#include "../FixedMap/AVLTree.hpp"
 #include "../FixedMap/BalancedTreeFactory.hpp"
-#include "../FixedMap/RedBlackTree.hpp"
 #include <gtest/gtest.h>
+#include <thread>
+#include <vector>
+#include <string>
 
 namespace ESTL {
 
     class BalancedTreeTest : public ::testing::TestWithParam<TreeType> {
     protected:
-        const size_t DEFAULT_CAPACITY = 10;
+        const size_t DEFAULT_CAPACITY = 5000;
         std::unique_ptr<BalancedTree<int, std::string>> tree;
+        TreeNode<int, std::string> *buffer;
 
         void SetUp() override {
-            tree = BalancedTreeFactory<int, std::string>::createTree(GetParam(), DEFAULT_CAPACITY);
+            buffer = new TreeNode<int, std::string>[DEFAULT_CAPACITY];
+            tree = BalancedTreeFactory<int, std::string>::createTree(GetParam
+                                                                     (), buffer, DEFAULT_CAPACITY);
+        }
+
+        void TearDown() override {
+          delete [] buffer;
         }
     };
 
@@ -109,7 +117,58 @@ namespace ESTL {
         EXPECT_EQ(tree->size(), 0);
     }
 
-    INSTANTIATE_TEST_SUITE_P(TreeTypes, BalancedTreeTest, ::testing::Values(TreeType::RedBlack, TreeType::AVL));
+    TEST_P(BalancedTreeTest, MultiThreadedInsertErase) {
+        const int numThreads = 5;
+        const int numOperations = 1000;
+
+        auto insertTask = [this](int start, int end) {
+            for (int i = start; i < end; ++i) {
+              printf("insertTask %d\n", i);
+                tree->insert(i, "value" + std::to_string(i));
+            }
+        };
+
+        auto eraseTask = [this](int start, int end) {
+            for (int i = start; i < end; ++i) {
+              printf("eraseTask %d\n", i);
+              tree->erase(i);
+            }
+        };
+
+
+        this->tree->clear();
+
+        std::vector<std::thread> threads;
+
+        // Launch threads to perform insertions
+        for (int i = 0; i < numThreads; ++i) {
+            threads.emplace_back(insertTask, i * numOperations, (i + 1) * numOperations);
+        }
+
+        // Wait for all insertion threads to finish
+        for (auto &thread : threads) {
+            thread.join();
+        }
+
+        EXPECT_EQ(tree->size(), numThreads * numOperations);
+
+        threads.clear();
+
+        // Launch threads to perform deletions
+        for (int i = 0; i < numThreads; ++i) {
+            threads.emplace_back(eraseTask, i * numOperations, (i + 1) * numOperations);
+        }
+
+        // Wait for all deletion threads to finish
+        for (auto &thread : threads) {
+            thread.join();
+        }
+
+        EXPECT_EQ(tree->size(), 0);
+    }
+
+    INSTANTIATE_TEST_SUITE_P(TreeTypes, BalancedTreeTest, ::testing::Values
+                             (TreeType::RedBlack, TreeType::AVL));
 
 
 }// namespace ESTL
